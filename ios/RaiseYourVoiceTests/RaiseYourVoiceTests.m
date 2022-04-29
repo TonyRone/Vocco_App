f40cef1d2271b3dc9d889b4d35d5c21f37c412ec
promarat <ace1998116@gmail.com>
2022-04-29 19:59:14 +0900

    s

--- ios/RaiseYourVoiceTests/RaiseYourVoiceTests.m
+++ ios/RaiseYourVoiceTests/RaiseYourVoiceTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface VoccoTests : XCTestCase
+@interface RaiseYourVoiceTests : XCTestCase
 
 @end
 
-@implementation VoccoTests
+@implementation RaiseYourVoiceTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
