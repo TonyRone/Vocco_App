c7685c3cbae24b116c089556f2563634d75665f9
promarat <ace1998116@gmail.com>
2022-07-15 02:24:29 +0900

    s

--- screens/component/MessageContent.js
+++ screens/component/MessageContent.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, Image, Modal, Pressable } from "react-native";
+import { View, Text, Image, Modal, Pressable, TouchableOpacity } from "react-native";
 import AutoHeightImage from 'react-native-auto-height-image';
 import { DescriptionText } from "./DescriptionText";
 import { useSelector } from 'react-redux';
@@ -12,7 +12,6 @@ import '../../language/i18n';
 import LinearGradient from "react-native-linear-gradient";
 import { SvgXml } from "react-native-svg";
 import blackReplySvg from '../../assets/chat/black-reply-icon.svg';
-import { TouchableOpacity } from "react-native-gesture-handler";
 import NavigationService from "../../services/NavigationService";
 
 export const MessageContent = ({
@@ -35,49 +34,54 @@ export const MessageContent = ({
 
   return (
     info.type == 'voice' ?
-      <LinearGradient
-        style={
-          {
-            padding: 8,
-            paddingRight: 8,
-            paddingLeft: 0,
-            borderBottomLeftRadius: 16,
-            borderBottomRightRadius: 16,
-            borderTopLeftRadius: isSender ? 16 : 8,
-            borderTopRightRadius: isSender ? 8 : 16,
+      <View style={{
+        height:80,
+        justifyContent:'center'
+      }}>
+        <LinearGradient
+          style={
+            {
+              padding: 8,
+              paddingRight: 8,
+              paddingLeft: 0,
+              borderBottomLeftRadius: 16,
+              borderBottomRightRadius: 16,
+              borderTopLeftRadius: isSender ? 16 : 8,
+              borderTopRightRadius: isSender ? 8 : 16,
+            }
           }
-        }
-        start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }}
-        colors={isSender ? ['#D89DF4', '#B35CF8', '#8229F4'] : ['#FFF', '#FFF', '#FFF']}
-      >
-        <VoicePlayer
-          voiceUrl={info?.file.url}
-          playBtn={true}
-          waveColor={['#E4CAFC', '#E4CAFC', '#E4CAFC']}
-          playing={false}
-          height={isAnswer ? 20 : 25}
-          playBtnSize={isAnswer ? 12 : 10}
-          startPlay={() => { }}
-          stopPlay={() => { }}
-          tinWidth={windowWidth / (isAnswer ? 350 : 300)}
-          mrg={windowWidth / (isAnswer ? 850 : 730)}
-          duration={info.duration * 1000}
-        />
-        <View style={[styles.rowSpaceBetween, { paddingLeft: 16, paddingRight: 8, marginTop: 6 }]}>
-          <DescriptionText
-            text={new Date(info.duration * 1000).toISOString().substr(14, 5)}
-            lineHeight={12}
-            fontSize={11}
-            color={isSender ? '#FFF' : 'rgba(59, 31, 82, 0.6)'}
+          start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }}
+          colors={isSender ? ['#D89DF4', '#B35CF8', '#8229F4'] : ['#FFF', '#FFF', '#FFF']}
+        >
+          <VoicePlayer
+            voiceUrl={info?.file.url}
+            playBtn={true}
+            waveColor={['#E4CAFC', '#E4CAFC', '#E4CAFC']}
+            playing={false}
+            height={isAnswer ? 20 : 25}
+            playBtnSize={isAnswer ? 12 : 10}
+            startPlay={() => { }}
+            stopPlay={() => { }}
+            tinWidth={windowWidth / (isAnswer ? 350 : 300)}
+            mrg={windowWidth / (isAnswer ? 850 : 730)}
+            duration={info.duration * 1000}
           />
-          <DescriptionText
-            text={new Date(localTime).toISOString().substr(11, 5)}
-            lineHeight={12}
-            fontSize={11}
-            color={isSender ? '#FFF' : 'rgba(59, 31, 82, 0.6)'}
-          />
-        </View>
-      </LinearGradient>
+          <View style={[styles.rowSpaceBetween, { paddingLeft: 16, paddingRight: 8, marginTop: 6 }]}>
+            <DescriptionText
+              text={new Date(info.duration * 1000).toISOString().substr(14, 5)}
+              lineHeight={12}
+              fontSize={11}
+              color={isSender ? '#FFF' : 'rgba(59, 31, 82, 0.6)'}
+            />
+            <DescriptionText
+              text={new Date(localTime).toISOString().substr(11, 5)}
+              lineHeight={12}
+              fontSize={11}
+              color={isSender ? '#FFF' : 'rgba(59, 31, 82, 0.6)'}
+            />
+          </View>
+        </LinearGradient>
+      </View>
       :
       info.type == 'emoji' ?
         <View style={{
@@ -123,6 +127,7 @@ export const MessageContent = ({
             />
             <View style={{
               marginLeft: 13,
+              height:182,
               borderRadius: 20,
               borderWidth: 3,
               borderColor: '#FFF',
