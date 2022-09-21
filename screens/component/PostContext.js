9cf11da446b508f8ba644494924267758483074f
promarat <ace1998116@gmail.com>
2022-09-21 10:19:42 +0300

    s

--- screens/component/PostContext.js
+++ screens/component/PostContext.js
@@ -24,10 +24,11 @@ import ciBackSvg from '../../assets/discover/context/back.svg';
 import ciTrashSvg from '../../assets/discover/context/trash.svg';
 import ciViolenceSvg from '../../assets/discover/context/violence.svg';
 import VoiceService from '../../services/VoiceService';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { styles } from '../style/Common';
 import { useTranslation } from 'react-i18next';
 import '../../language/i18n';
+import { setRefreshState } from '../../store/actions';
 
 export const PostContext = ({
   postInfo,
@@ -37,12 +38,13 @@ export const PostContext = ({
 }) => {
 
   const { t, i18n } = useTranslation();
+  const dispatch = useDispatch();
 
   const [showReport, setShowReport] = useState(false);
   const [showModal, setShowModal] = useState(true);
   const [playstatus, setPlaystatus] = useState(false);
 
-  let { user } = useSelector((state) => {
+  let { user, refreshState } = useSelector((state) => {
     return (
       state.user
     )
@@ -67,11 +69,18 @@ export const PostContext = ({
     });
   }, []);
 
+  const onBlockUser = () => {
+    VoiceService.blockUser(postInfo.user.id).then(res => {
+      dispatch(setRefreshState(!refreshState));
+      closeModal();
+    })
+  }
+
   return (
     <Modal
       animationType="slide"
       transparent={true}
-      elevation = {5}
+      elevation={5}
       visible={showModal}
       onRequestClose={() => {
         closeModal();
@@ -79,7 +88,7 @@ export const PostContext = ({
     >
       <Pressable onPressOut={closeModal} style={styles.swipeModal}>
         <View
-          style={ { marginTop: "25%" }}
+          style={{ marginTop: "25%" }}
         >
           <VoiceItem
             info={postInfo}
@@ -186,10 +195,12 @@ export const PostContext = ({
             >
               <Pressable
                 style={styles.contextMenu}
+                onPress={() => onBlockUser()}
               >
                 <TitleText
-                  text={t("Spam")}
+                  text={t("Block")}
                   fontSize={17}
+                  color='#FFF'
                   fontFamily="SFProDisplay-Regular"
                 />
                 <SvgXml
@@ -202,49 +213,7 @@ export const PostContext = ({
                 style={styles.contextMenu}
               >
                 <TitleText
-                  text={t("Fake Account")}
-                  fontSize={17}
-                  fontFamily="SFProDisplay-Regular"
-                />
-                <SvgXml
-                  width={20}
-                  height={20}
-                  xml={ciFakeSvg}
-                />
-              </Pressable>
-              <Pressable
-                style={styles.contextMenu}
-              >
-                <TitleText
-                  text={t("Violence")}
-                  fontSize={17}
-                  fontFamily="SFProDisplay-Regular"
-                />
-                <SvgXml
-                  width={20}
-                  height={20}
-                  xml={ciViolenceSvg}
-                />
-              </Pressable>
-              <Pressable
-                style={styles.contextMenu}
-              >
-                <TitleText
-                  text={t("Child Abuse")}
-                  fontSize={17}
-                  fontFamily="SFProDisplay-Regular"
-                />
-                <SvgXml
-                  width={20}
-                  height={20}
-                  xml={ciAbuseSvg}
-                />
-              </Pressable>
-              <Pressable
-                style={styles.contextMenu}
-              >
-                <TitleText
-                  text={t("Other")}
+                  text={t("Report")}
                   fontSize={17}
                   fontFamily="SFProDisplay-Regular"
                 />
