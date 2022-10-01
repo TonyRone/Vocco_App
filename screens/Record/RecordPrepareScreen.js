ab5d0b7fbeebeb7fa2f22d89eb98bd506222a56a
promarat <ace1998116@gmail.com>
2022-10-01 12:08:01 +0300

    s

--- screens/Record/RecordPrepareScreen.js
+++ screens/Record/RecordPrepareScreen.js
@@ -74,163 +74,170 @@ const RecordPrepareScreen = (props) => {
   }
 
   return (
-    <Pressable style={{ width: windowWidth, flex: 1, backgroundColor: '#FFFFFF' }} onPress={() => Keyboard.dismiss()}>
-      <View>
-        <TouchableOpacity style={{ marginTop: Platform.OS == 'ios' ? 50 : 20, marginLeft: 21 }} onPress={() => props.navigation.goBack()}>
-          <SvgXml width={14} height={14} xml={closeSvg} />
-        </TouchableOpacity>
-      </View>
-      <View style={{ alignItems: "center", marginTop: windowHeight / 812 * 17 }}>
-        <TextInput
-          placeholder={t("Your title")}
-          placeholderTextColor="#3612521E"
-          color="#361252"
-          textAlign={'center'}
-          autoFocus={true}
-          value={voiceTitle}
-          onChangeText={(s) => { s.length <= 25 ? setVoiceTitle(s) : null; setWarning(false) }}
-          fontFamily="SFProDisplay-Regular"
-          fontSize={34}
-          lineHeight={41}
-          marginTop={5}
-          letterSpaceing={5}
-        />
-      </View>
-      {warning === true ? <View style={{ width: "100%", alignItems: "center", marginTop: windowHeight / 812 * 21 }}>
-        <View style={{
-          backgroundColor: "#E41717",
-          // backgroundColor: "#FFF",
-          borderRadius: 16,
-          paddingHorizontal: 34,
-          paddingVertical: 10,
-          shadowColor: 'rgba(244, 13, 13, 1)',
-          elevation: 10,
-          shadowOffset: { width: 0, height: 5 },
-          shadowRadius: 8
-        }}>
-          <Text style={{ color: "white", fontWeight: "500", fontSize: 15, lineHeight: 18, fontFamily: "SFProDisplay-Medium" }}>{t("Add a title to your story!")}</Text>
-        </View>
-      </View> : <View></View>}
-      <View style={{ width: "100%", alignItems: "center", marginTop: !warning ? windowHeight / 812 * 70 : windowHeight / 812 * 25 }}>
-        {source ? <View style={{
-          position: "relative"
-        }}>
-          <Image source={{ uri: source.path }} style={{
-            width: windowWidth / 375 * 234,
-            height: windowWidth / 375 * 234,
-            borderRadius: 16
-          }} />
-          <TouchableOpacity style={{
-            width: 34,
-            height: 34,
-            position: "absolute",
-            backgroundColor: "#F8F0FF",
-            borderRadius: 18,
-            bottom: 10,
-            right: 10,
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-            onPress={() => setPickModal(true)}
-          >
-            <SvgXml width={11} height={14} xml={editImageSvg} />
+    <KeyboardAvoidingView
+      style={{
+        backgroundColor: '#FFF',
+        flex: 1
+      }}
+    >
+      <Pressable style={{ width: windowWidth, flex: 1, backgroundColor: '#FFFFFF' }} onPress={() => Keyboard.dismiss()}>
+        <View>
+          <TouchableOpacity style={{ marginTop: Platform.OS == 'ios' ? 50 : 20, marginLeft: 21 }} onPress={() => props.navigation.goBack()}>
+            <SvgXml width={14} height={14} xml={closeSvg} />
           </TouchableOpacity>
-        </View> :
+        </View>
+        <View style={{ alignItems: "center", marginTop: windowHeight / 812 * 17 }}>
+          <TextInput
+            placeholder={t("Your title")}
+            placeholderTextColor="#3612521E"
+            color="#361252"
+            textAlign={'center'}
+            autoFocus={true}
+            value={voiceTitle}
+            onChangeText={(s) => { s.length <= 25 ? setVoiceTitle(s) : null; setWarning(false) }}
+            fontFamily="SFProDisplay-Regular"
+            fontSize={34}
+            lineHeight={41}
+            marginTop={5}
+            letterSpaceing={5}
+          />
+        </View>
+        {warning === true ? <View style={{ width: "100%", alignItems: "center", marginTop: windowHeight / 812 * 21 }}>
           <View style={{
-            width: windowWidth / 375 * 234,
-            height: windowWidth / 375 * 234,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center"
+            backgroundColor: "#E41717",
+            // backgroundColor: "#FFF",
+            borderRadius: 16,
+            paddingHorizontal: 34,
+            paddingVertical: 10,
+            shadowColor: 'rgba(244, 13, 13, 1)',
+            elevation: 10,
+            shadowOffset: { width: 0, height: 5 },
+            shadowRadius: 8
           }}>
-            <TouchableOpacity onPress={() => setPickModal(true)}>
-              <SvgXml width={93} height={93} xml={uploadSvg} />
+            <Text style={{ color: "white", fontWeight: "500", fontSize: 15, lineHeight: 18, fontFamily: "SFProDisplay-Medium" }}>{t("Add a title to your story!")}</Text>
+          </View>
+        </View> : <View></View>}
+        <View style={{ width: "100%", alignItems: "center", marginTop: !warning ? windowHeight / 812 * 70 : windowHeight / 812 * 25 }}>
+          {source ? <View style={{
+            position: "relative"
+          }}>
+            <Image source={{ uri: source.path }} style={{
+              width: windowWidth / 375 * 234,
+              height: windowWidth / 375 * 234,
+              borderRadius: 16
+            }} />
+            <TouchableOpacity style={{
+              width: 34,
+              height: 34,
+              position: "absolute",
+              backgroundColor: "#F8F0FF",
+              borderRadius: 18,
+              bottom: 10,
+              right: 10,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+              onPress={() => setPickModal(true)}
+            >
+              <SvgXml width={11} height={14} xml={editImageSvg} />
             </TouchableOpacity>
-            <Text style={{ marginTop: windowHeight / 812 * 24, fontFamily: "SFProDisplay-Regular", fontSize: 12, lineHeight: 28, color: "#361252" }}>{t("Add a picture to illustrate your story!")}</Text>
-          </View>
-        }
-      </View>
-      <View style={{ marginLeft: 35, marginTop: windowHeight / 812 * 50, flexDirection: "row", alignItems: 'center' }}>
-        <Text style={{ fontWeight: "400", fontSize: 17, lineHeight: 28, color: "#361252", fontFamily: "SFProDisplay-Regular" }}>Where did it happen? </Text>
-        <Text style={{ fontWeight: "400", fontSize: 17, lineHeight: 28, color: "#36125232", fontFamily: "SFProDisplay-Regular" }}>({t('optionnal')})</Text>
-      </View>
-      <View style={{ paddingHorizontal: 27, marginTop: 10 }}>
-        <Pressable style={{
-          width: "100%",
-          flexDirection: "row",
-          borderRadius: 24,
-          paddingVertical: 8,
-          paddingHorizontal: 13,
-          alignItems: "center",
-          borderWidth: 1,
-          borderColor: "rgb(0, 0, 0)",
-          backgroundColor: "rgba(242, 240, 245, 1)"
-        }}
-          onPress={() => setShowCityModal(true)}
-        >
-          <SvgXml width={15} height={15} xml={globalSvg} />
-          <View style={{ width: "100%", height: 15, marginLeft: 16, borderLeftWidth: storyAddress ? 0 : 1, borderLeftColor: "rgb(0, 0, 0)" }}>
-            {storyAddress ? <Text style={{ fontWeight: "400", fontSize: 17, lineHeight: 17, marginLeft: 2 }}>{storyAddress.description}</Text> : <Text style={{ fontWeight: "400", fontSize: 17, lineHeight: 17, marginLeft: 2 }}>{t('Search')}...</Text>}
-          </View>
-        </Pressable>
-      </View>
-      <View style={{ position: 'absolute', width: "100%", alignItems: "center", bottom: 70 }}>
-        <TouchableOpacity onPress={() => onClickNext()}>
-          <LinearGradient
-            style={
-              {
-                borderRadius: 30,
-                alignItems: 'center',
-                flexDirection: 'row',
-                paddingLeft: 29,
-                paddingRight: 22,
-                paddingVertical: 12
-              }
-            }
-            start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }}
-            colors={['#D89DF4', '#B35CF8', '#8229F4']}
+          </View> :
+            <View style={{
+              width: windowWidth / 375 * 234,
+              height: windowWidth / 375 * 234,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center"
+            }}>
+              <TouchableOpacity onPress={() => setPickModal(true)}>
+                <SvgXml width={93} height={93} xml={uploadSvg} />
+              </TouchableOpacity>
+              <Text style={{ marginTop: windowHeight / 812 * 24, fontFamily: "SFProDisplay-Regular", fontSize: 12, lineHeight: 28, color: "#361252" }}>{t("Add a picture to illustrate your story!")}</Text>
+            </View>
+          }
+        </View>
+        <View style={{ marginLeft: 35, marginTop: windowHeight / 812 * 50, flexDirection: "row", alignItems: 'center' }}>
+          <Text style={{ fontWeight: "400", fontSize: 17, lineHeight: 28, color: "#361252", fontFamily: "SFProDisplay-Regular" }}>Where did it happen? </Text>
+          <Text style={{ fontWeight: "400", fontSize: 17, lineHeight: 28, color: "#36125232", fontFamily: "SFProDisplay-Regular" }}>({t('optionnal')})</Text>
+        </View>
+        <View style={{ paddingHorizontal: 27, marginTop: 10 }}>
+          <Pressable style={{
+            width: "100%",
+            flexDirection: "row",
+            borderRadius: 24,
+            paddingVertical: 8,
+            paddingHorizontal: 13,
+            alignItems: "center",
+            borderWidth: 1,
+            borderColor: "rgb(0, 0, 0)",
+            backgroundColor: "rgba(242, 240, 245, 1)"
+          }}
+            onPress={() => setShowCityModal(true)}
           >
-            <Text style={{
-              fontWeight: "500",
-              fontSize: 17,
-              lineHeight: 28,
-              color: "white",
-              marginRight: 8
-            }}>{t('Next step')}</Text>
-            <SvgXml width={32} height={32} xml={nextSvg} />
-          </LinearGradient>
-        </TouchableOpacity>
-      </View>
-      {pickModal && <PickImage
-        onSetImageSource={(img) => onSetSource(img)}
-        onCloseModal={() => setPickModal(false)}
-      />}
-      <Modal
-        animationType='slide'
-        visible={showCityModal}
-        onRequestClose={() => setShowCityModal(!showCityModal)}
-      >
-        <Pressable onPress={() => setShowCityModal(false)} style={[styles.centeredView, { justifyContent: 'flex-start', paddingHorizontal: 8, backgroundColor: 'white' }]}>
-          <View style={{ width: windowWidth, height: windowHeight, paddingTop: 50 }}>
-            <GooglePlacesAutocomplete
-              placeholder='Search'
-              onPress={(data, details = null) => {
-                // 'details' is provided when fetchDetails = true
-                setStoryAddress(data);
-                setShowCityModal(false);
-              }}
-              query={{
-                key: 'AIzaSyBLImCd0n7ztnIyLHwkMbZC9APGTDEqgX4',
-                language: 'en'
-              }}
-            />
-          </View>
-        </Pressable>
-      </Modal>
+            <SvgXml width={15} height={15} xml={globalSvg} />
+            <View style={{ width: "100%", height: 15, marginLeft: 16, borderLeftWidth: storyAddress ? 0 : 1, borderLeftColor: "rgb(0, 0, 0)" }}>
+              {storyAddress ? <Text style={{ fontWeight: "400", fontSize: 17, lineHeight: 17, marginLeft: 2 }}>{storyAddress.description}</Text> : <Text style={{ fontWeight: "400", fontSize: 17, lineHeight: 17, marginLeft: 2 }}>{t('Search')}...</Text>}
+            </View>
+          </Pressable>
+        </View>
+        <View style={{ position: 'absolute', width: "100%", alignItems: "center", bottom: 70 }}>
+          <TouchableOpacity onPress={() => onClickNext()}>
+            <LinearGradient
+              style={
+                {
+                  borderRadius: 30,
+                  alignItems: 'center',
+                  flexDirection: 'row',
+                  paddingLeft: 29,
+                  paddingRight: 22,
+                  paddingVertical: 12
+                }
+              }
+              start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }}
+              colors={['#D89DF4', '#B35CF8', '#8229F4']}
+            >
+              <Text style={{
+                fontWeight: "500",
+                fontSize: 17,
+                lineHeight: 28,
+                color: "white",
+                marginRight: 8
+              }}>{t('Next step')}</Text>
+              <SvgXml width={32} height={32} xml={nextSvg} />
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+        {pickModal && <PickImage
+          onSetImageSource={(img) => onSetSource(img)}
+          onCloseModal={() => setPickModal(false)}
+        />}
+        <Modal
+          animationType='slide'
+          visible={showCityModal}
+          onRequestClose={() => setShowCityModal(!showCityModal)}
+        >
+          <Pressable onPress={() => setShowCityModal(false)} style={[styles.centeredView, { justifyContent: 'flex-start', paddingHorizontal: 8, backgroundColor: 'white' }]}>
+            <View style={{ width: windowWidth, height: windowHeight, paddingTop: 50 }}>
+              <GooglePlacesAutocomplete
+                placeholder='Search'
+                onPress={(data, details = null) => {
+                  // 'details' is provided when fetchDetails = true
+                  setStoryAddress(data);
+                  setShowCityModal(false);
+                }}
+                query={{
+                  key: 'AIzaSyBLImCd0n7ztnIyLHwkMbZC9APGTDEqgX4',
+                  language: 'en'
+                }}
+              />
+            </View>
+          </Pressable>
+        </Modal>
+      </Pressable>
       {Platform.OS == 'ios' && <KeyboardSpacer />}
-    </Pressable>
+    </KeyboardAvoidingView>
   )
 }
 
