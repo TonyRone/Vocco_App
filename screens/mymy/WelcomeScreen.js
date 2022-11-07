edca42205fd614b5c1a92004d2711b7d82288410
TonyRone <ace1998116@gmail.com>
2022-11-07 15:27:16 +0300

    s

--- screens/mymy/WelcomeScreen.js
+++ screens/mymy/WelcomeScreen.js
@@ -78,7 +78,7 @@ const WelcomeScreen = (props) => {
             </View>
             { isWarning && <View style={{ position: "absolute", width: "100%", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
                 <View style={{
-                paddingHorizontal: 20,
+                paddingHorizontal: 30,
                 paddingVertical: 16,
                 backgroundColor: "#E41717",
                 borderRadius: 16,
