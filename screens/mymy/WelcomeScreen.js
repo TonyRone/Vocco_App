76edde9d5b783826b7edbb80a088a419fd53858f
TonyRone <ace1998116@gmail.com>
2022-11-07 15:21:30 +0300

    s

--- screens/mymy/WelcomeScreen.js
+++ screens/mymy/WelcomeScreen.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
-import { View, ImageBackground, Image, Text, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, ImageBackground, Image, Text, TouchableOpacity, Linking } from 'react-native';
 import { TitleText } from '../component/TitleText';
 import { DescriptionText } from '../component/DescriptionText';
 import { useTranslation } from 'react-i18next';
 import '../../language/i18n';
+import CheckBox from 'react-native-check-box';
 
 import { styles } from '../style/Welcome';
 
@@ -13,6 +14,9 @@ const WelcomeScreen = (props) => {
 
     useEffect(() => {
     }, [])
+
+    const [isSelected, setIsSelected] = useState(false);
+    const [isWarning, setIsWarning] = useState(false);
 
     return (
         <View style={styles.container}>
@@ -34,25 +38,36 @@ const WelcomeScreen = (props) => {
                             style={styles.rowJustifyCenter}
                         >
                             <TitleText
-                                text={t("Worldwide stories. Everyday.")}
+                                text={t("Worldwide stories")}
                                 marginTop={20}
                             />
                         </View>
                         <DescriptionText
-                            text={t("And share yours, too!")}
+                            text={t("Everyday")}
                             textAlign="center"
-                            marginTop={20}
+                            marginTop={15}
                         />
+                        <View style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", marginTop: 15 }}>
+                            <CheckBox
+                                isChecked={isSelected}
+                                onClick={() => {setIsSelected(!isSelected); setIsWarning(false)}}
+                                // style={{ width: 12, height: 12 }}
+                            />
+                            <Text style={{ color: "#000000", fontSize: 13, lineHeight: 13, marginLeft: 3 }}>{t("I accept the")}</Text>
+                            <TouchableOpacity style={{ marginLeft: 3 }} onPress={() => Linking.openURL("https://vocco.ai/privacy")}>
+                                <Text style={{ color: "#A24EE4", fontSize: 13, lineHeight: 13 }}>{t("terms of use and privacy policy")}</Text>
+                            </TouchableOpacity>
+                        </View>
                         <View style={[styles.rowSpaceEvenly, { marginBottom: 25, marginTop: 50 }]}>
                             <TouchableOpacity
                                 style={styles.registerButton}
-                                onPress={() => props.navigation.navigate("MainName")}
+                                onPress={() => { isSelected ? props.navigation.navigate("MainName") : setIsWarning(true)}}
                             >
                                 <Text style={styles.registerText} >{t("Get Started")}</Text>
                             </TouchableOpacity>
                             <TouchableOpacity
                                 style={styles.loginButton}
-                                onPress={() => props.navigation.navigate('PhoneLogin')}
+                                onPress={() => { isSelected ? props.navigation.navigate('PhoneLogin') : setIsWarning(true)}}
                                 //onPress={() => props.navigation.navigate('Login')}
                             >
                                 <Text style={styles.loginText}>{t("Log In")}</Text>
@@ -61,6 +76,22 @@ const WelcomeScreen = (props) => {
                     </View>
                 </ImageBackground>
             </View>
+            { isWarning && <View style={{ position: "absolute", width: "100%", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+                <View style={{
+                paddingHorizontal: 20,
+                paddingVertical: 16,
+                backgroundColor: "#E41717",
+                borderRadius: 16,
+                shadowColor: 'rgba(244, 13, 13, 0.47)',
+                elevation: 10,
+                shadowOffset: { width: 0, height: 5.22 },
+                shadowOpacity: 0.5,
+                shadowRadius: 16,
+                top: 20
+            }}>
+                <Text style={{ color: "white", fontSize: 15, lineHeight: 18 }}>{t("You must agree our terms")}</Text>
+            </View>
+            </View> }
         </View>
     );
 };
