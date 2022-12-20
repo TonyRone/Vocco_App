08c2eaea06e1d1bad4024a8504c5ec1b066aa316
promarat <ace1998116@gmail.com>
2022-12-20 23:21:21 +0300

    remove notification

--- App.js
+++ App.js
@@ -78,7 +78,7 @@ import SelectTopicScreen from './screens/PhoneNumberLogin/SelectTopicScreen';
 import AddFriendScreen from './screens/PhoneNumberLogin/AddFriendScreen';
 import ShareStoryScreen from './screens/mymy/ShareStoryScreen';
 import HoldRecordScreen from './screens/Record/HoldRecordScreen';
-import { NotificationServices } from './screens/mymy';
+// import { NotificationServices } from './screens/mymy';
 import CalendarScreen from './screens/Home/CalendarScreen';
 
 
@@ -492,7 +492,7 @@ export default App = () => {
           NavigationService.setTopLevelNavigator(navigatorRef);
         }}
       />
-      <NotificationServices />
+      {/* <NotificationServices /> */}
     </Provider>
   );
 };
